2a01cb0729e1deeb05a286d328479c31a83c2dba
Danyel Bayraktar <danyel@webhippie.de>
2015-08-02 16:38:55 +0200

    Added example for usage of low-level bindings.

---
+++ unrar_sys/examples/lister.rs
@@ -0,0 +1,79 @@
+extern crate unrar_sys;
+extern crate libc;
+
+use unrar_sys::*;
+use libc::{c_int, c_uint, c_long};
+use std::str;
+
+use std::ffi::{CStr, CString};
+use std::fs;
+use std::io::Write;
+
+fn main() {
+    let args = std::env::args();
+    let mut stderr = std::io::stderr();
+    let file = args.skip(1).next().unwrap_or_else(|| {
+        writeln!(&mut stderr, "Please pass an archive as argument!").unwrap();
+        std::process::exit(0)
+    });
+    extern "C" fn callback(msg: c_uint, user_data: c_long, p1: c_long, p2: c_long) -> c_int {
+        match (msg, p2) {
+            (UCM_CHANGEVOLUME, RAR_VOL_ASK) => {
+                let ptr = p1 as *const _;
+                let next = str::from_utf8(unsafe { CStr::from_ptr(ptr) }.to_bytes()).unwrap();
+                let our_string = unsafe { &mut *(user_data as *mut String) };
+                our_string.push_str(next);
+                -1
+            },
+            (UCM_CHANGEVOLUME, RAR_VOL_NOTIFY) => 1,
+            _ => 0
+        }
+    }
+    let mut data = OpenArchiveData::new(
+        CString::new(file).unwrap().as_ptr(),
+        RAR_OM_LIST_INCSPLIT
+    );
+    let handle = unsafe {RAROpenArchive(&mut data as *mut _)};
+    assert_eq!(data.open_result, 0);
+    assert_eq!(handle.is_null(), false);
+    let mut next_path = String::with_capacity(1024);
+    unsafe { RARSetCallback(handle, callback, &mut next_path as *mut String as c_long) };
+    let mut header = HeaderData::default();
+    let mut result = 0;
+    let mut process_result;
+    let mut first = true;
+    while result == 0 {
+        result = unsafe {RARReadHeader(handle, &mut header as *mut _)};
+        if result != ERAR_SUCCESS {
+            if result != ERAR_END_ARCHIVE {
+                writeln!(&mut stderr, "Error opening: {}", result).unwrap();
+            }
+            break;
+        }
+        if first && header.flags & RHDF_SPLITBEFORE != 0 {
+            writeln!(&mut stderr, "Not beginning of archive! Still continuing").unwrap();
+        }
+        first = false;
+        let s = str::from_utf8(unsafe {
+            CStr::from_ptr(header.filename.as_ptr())
+        }.to_bytes()).unwrap();
+        process_result = unsafe { RARProcessFile(
+            handle,
+            RAR_SKIP,
+            0 as *const _,
+            0 as *const _
+        ) };
+        println!("{}", s);
+        println!("{}", process_result);
+        match process_result {
+            ERAR_SUCCESS => (),
+            ERAR_EOPEN => {
+                if let Err(err) = fs::metadata(&next_path) {
+                    writeln!(&mut stderr, "Couldn't find volume {}: {}", next_path, err).unwrap();
+                    break;
+                }
+            },
+            x => writeln!(&mut stderr, "Error: {}", x).unwrap()
+        }
+    }
+}
