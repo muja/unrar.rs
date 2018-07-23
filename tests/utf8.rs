38e9594bd1411ab67bb30a73a7def820839feac9
casret <github@casret.com>
2018-07-23 00:48:03 -0700

    Update utf8.rs

--- tests/utf8.rs
+++ tests/utf8.rs
@@ -6,7 +6,7 @@ use std::fs::File;
 use std::io::prelude::*;
 
 #[test]
-fn version_list() {
+fn foobar_list() {
     let mut entries = unrar::Archive::new("data/utf8.rar".into()).list().unwrap();
     assert_eq!(entries.next().unwrap().unwrap().filename, "foo—bar");
 }
