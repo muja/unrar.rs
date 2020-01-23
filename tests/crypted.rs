3640ef5bf864ad88d16a8502de9a6e3e72e2e488
vjoki <vjoki@zv.fi>
2020-01-23 16:14:52 +0200

    Add tests for an archive with encrypted headers

--- tests/crypted.rs
+++ tests/crypted.rs
@@ -39,3 +39,35 @@ fn version_cat() {
     file.read_to_string(&mut s).unwrap();
     assert_eq!(s, "target\nCargo.lock\n");
 }
+
+#[test]
+fn list_encrypted_headers() {
+    let mut entries = Archive::with_password("data/comment-hpw-password.rar", "password").unwrap()
+        .list().unwrap();
+    assert_eq!(entries.next().unwrap().unwrap().filename, PathBuf::from(".gitignore"));
+}
+
+#[test]
+fn no_password_list_encrypted_headers() {
+    // Password needed in order to list contents
+    let mut entries = Archive::new("data/comment-hpw-password.rar").unwrap()
+        .list().unwrap();
+    let err = entries.next().unwrap().unwrap_err();
+    assert_eq!(err.code, Code::MissingPassword);
+    assert_eq!(err.when, When::Read);
+    assert!(err.data.is_none());
+}
+
+#[test]
+fn extract_encrypted_headers() {
+    let t = TempDir::new("unrar").unwrap();
+    Archive::with_password("data/comment-hpw-password.rar", "password").unwrap()
+        .extract_to(t.path())
+        .unwrap()
+        .process()
+        .unwrap();
+    let mut file = File::open(t.path().join(".gitignore")).unwrap();
+    let mut s = String::new();
+    file.read_to_string(&mut s).unwrap();
+    assert_eq!(s, "target\nCargo.lock\n");
+}
