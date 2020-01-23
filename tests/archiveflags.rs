5158b49e2eab82a95344ba6c1c584994220e2dda
vjoki <vjoki@zv.fi>
2020-01-23 16:01:48 +0200

    Tweak ArchiveFlags and add tests

---
+++ tests/archiveflags.rs
@@ -0,0 +1,61 @@
+extern crate unrar;
+
+use unrar::Archive;
+use unrar::archive::VolumeInfo;
+
+#[test]
+fn volume() {
+    let archive = Archive::new("data/version.rar").unwrap().list().unwrap();
+    assert_eq!(archive.volume_info(), VolumeInfo::None);
+
+    let archive = Archive::new("data/archive.part1.rar").unwrap().list().unwrap();
+    assert_eq!(archive.volume_info(), VolumeInfo::First);
+
+    let archive = Archive::new("data/100M.part00002.rar").unwrap().list().unwrap();
+    assert_eq!(archive.volume_info(), VolumeInfo::Subsequent);
+}
+
+#[test]
+fn locked() {
+    let archive = Archive::new("data/locked.rar").unwrap().list().unwrap();
+    assert!(archive.is_locked());
+
+    let archive = Archive::new("data/version.rar").unwrap().list().unwrap();
+    assert!(!archive.is_locked());
+}
+
+#[test]
+fn recovery_record() {
+    let archive = Archive::new("data/recovery-record.rar").unwrap().list().unwrap();
+    assert!(archive.has_recovery_record());
+
+    let archive = Archive::new("data/version.rar").unwrap().list().unwrap();
+    assert!(!archive.has_recovery_record());
+}
+
+#[test]
+fn archive_comment() {
+    let archive = Archive::new("data/comment.rar").unwrap().list().unwrap();
+    assert!(archive.has_comment());
+
+    let archive = Archive::new("data/version.rar").unwrap().list().unwrap();
+    assert!(!archive.has_comment());
+}
+
+#[test]
+fn encrypted_headers() {
+    let archive = Archive::new("data/comment-hpw-password.rar").unwrap().list().unwrap();
+    assert!(archive.has_encrypted_headers());
+
+    let archive = Archive::new("data/version.rar").unwrap().list().unwrap();
+    assert!(!archive.has_encrypted_headers());
+}
+
+#[test]
+fn solid() {
+    let archive = Archive::new("data/solid.rar").unwrap().list().unwrap();
+    assert!(archive.is_solid());
+
+    let archive = Archive::new("data/version.rar").unwrap().list().unwrap();
+    assert!(!archive.is_solid());
+}
