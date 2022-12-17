8588825d365e37abf0fad360fc1223e7fb15772f
Danyel Bayraktar <vcs@danyel.io>
2022-12-17 21:29:20 +0100

    chore(dll): upgrade DLL version to 6.2.8

--- unrar_sys/build.rs
+++ unrar_sys/build.rs
@@ -10,10 +10,63 @@ fn main() {
     } else {
         println!("cargo:rustc-link-lib=pthread");
     }
+    let files: Vec<String> = [
+        "rar",
+        "strlist",
+        "strfn",
+        "pathfn",
+        "smallfn",
+        "global",
+        "file",
+        "filefn",
+        "filcreat",
+        "archive",
+        "arcread",
+        "unicode",
+        "system",
+        #[cfg(windows)]
+        "isnt",
+        "crypt",
+        "crc",
+        "rawread",
+        "encname",
+        "resource",
+        "match",
+        "timefn",
+        "rdwrfn",
+        "consio",
+        "options",
+        "errhnd",
+        "rarvm",
+        "secpassword",
+        "rijndael",
+        "getbits",
+        "sha1",
+        "sha256",
+        "blake2s",
+        "hash",
+        "extinfo",
+        "extract",
+        "volume",
+        "list",
+        "find",
+        "unpack",
+        "headers",
+        "threadpool",
+        "rs16",
+        "cmddata",
+        "ui",
+        "filestr",
+        "scantree",
+        "dll",
+        "qopen",
+    ].iter().map(|&s| format!("vendor/unrar/{s}.cpp")).collect();
     cc::Build::new()
         .cpp(true) // Switch to C++ library compilation.
         .opt_level(2)
         .warnings(false)
+        .flag("-std=c++11")
+        .flag_if_supported("-stdlib=libc++")
         .flag_if_supported("-fPIC")
         .flag_if_supported("-Wno-switch")
         .flag_if_supported("-Wno-parentheses")
@@ -29,53 +82,6 @@ fn main() {
         .define("_LARGEFILE_SOURCE", None)
         .define("RAR_SMP", None)
         .define("RARDLL", None)
-        .file("vendor/unrar/rar.cpp")
-        .file("vendor/unrar/strlist.cpp")
-        .file("vendor/unrar/strfn.cpp")
-        .file("vendor/unrar/pathfn.cpp")
-        .file("vendor/unrar/smallfn.cpp")
-        .file("vendor/unrar/global.cpp")
-        .file("vendor/unrar/file.cpp")
-        .file("vendor/unrar/filefn.cpp")
-        .file("vendor/unrar/filcreat.cpp")
-        .file("vendor/unrar/archive.cpp")
-        .file("vendor/unrar/arcread.cpp")
-        .file("vendor/unrar/unicode.cpp")
-        .file("vendor/unrar/system.cpp")
-        .file("vendor/unrar/isnt.cpp")
-        .file("vendor/unrar/crypt.cpp")
-        .file("vendor/unrar/crc.cpp")
-        .file("vendor/unrar/rawread.cpp")
-        .file("vendor/unrar/encname.cpp")
-        .file("vendor/unrar/resource.cpp")
-        .file("vendor/unrar/match.cpp")
-        .file("vendor/unrar/timefn.cpp")
-        .file("vendor/unrar/rdwrfn.cpp")
-        .file("vendor/unrar/consio.cpp")
-        .file("vendor/unrar/options.cpp")
-        .file("vendor/unrar/errhnd.cpp")
-        .file("vendor/unrar/rarvm.cpp")
-        .file("vendor/unrar/secpassword.cpp")
-        .file("vendor/unrar/rijndael.cpp")
-        .file("vendor/unrar/getbits.cpp")
-        .file("vendor/unrar/sha1.cpp")
-        .file("vendor/unrar/sha256.cpp")
-        .file("vendor/unrar/blake2s.cpp")
-        .file("vendor/unrar/hash.cpp")
-        .file("vendor/unrar/extinfo.cpp")
-        .file("vendor/unrar/extract.cpp")
-        .file("vendor/unrar/volume.cpp")
-        .file("vendor/unrar/list.cpp")
-        .file("vendor/unrar/find.cpp")
-        .file("vendor/unrar/unpack.cpp")
-        .file("vendor/unrar/headers.cpp")
-        .file("vendor/unrar/threadpool.cpp")
-        .file("vendor/unrar/rs16.cpp")
-        .file("vendor/unrar/cmddata.cpp")
-        .file("vendor/unrar/ui.cpp")
-        .file("vendor/unrar/filestr.cpp")
-        .file("vendor/unrar/scantree.cpp")
-        .file("vendor/unrar/dll.cpp")
-        .file("vendor/unrar/qopen.cpp")
+        .files(&files)
         .compile("libunrar.a");
 }
--- unrar_sys/vendor/unrar/version.hpp
+++ unrar_sys/vendor/unrar/version.hpp
@@ -1,6 +1,6 @@
-#define RARVER_MAJOR     5
-#define RARVER_MINOR    80
-#define RARVER_BETA      4
-#define RARVER_DAY      17
-#define RARVER_MONTH    11
-#define RARVER_YEAR   2019
+#define RARVER_MAJOR     6
+#define RARVER_MINOR    22
+#define RARVER_BETA      0
+#define RARVER_DAY      29
+#define RARVER_MONTH     5
+#define RARVER_YEAR   2023
