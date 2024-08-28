519956a9ef7bec40a6a3e29a0fc05b0b1ae9b6b4
Danyel <vcs@danyel.io>
2024-08-28 19:50:11 +0200

    tests: add test case to verify tempdir extraction is fixed
    Closes #34

--- tests/simple.rs
+++ tests/simple.rs
@@ -25,3 +25,19 @@ fn version_cat() {
     let s = String::from_utf8(bytes).unwrap();
     assert_eq!(s, "unrar-0.4.0");
 }
+
+#[test]
+fn extract_to_tempdir() {
+    // see https://github.com/muja/unrar.rs/issues/34
+    let file = "data/version.rar".to_owned();
+    let mut archive = unrar::Archive::new(&file).open_for_processing().expect("open archive");
+    let temp_path = tempfile::tempdir().expect("creating tempdir");
+    let temp_path = temp_path.path();
+    while let Some(header) = archive.read_header().expect("read header") {
+        let temp_file_path = temp_path.join(header.entry().filename.as_path());
+        archive = header.extract_to(temp_file_path.as_path()).expect("extract_to");
+    }
+    let entries = std::fs::read_dir(&temp_path).expect("read tempdir").collect::<Result<Vec<_>, _>>().unwrap();
+    assert!(entries.len() == 1);
+    assert!(entries[0].file_name() == "VERSION");
+}
